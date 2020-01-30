d1ed580f8f7438104a0b9bcaccfe71deea0d8857
Tyler Hunt <47220891+TyyHunt@users.noreply.github.com>
2020-01-30 05:42:36 -0700

    added ngFor for iterating through recipes

--- FirstAngularProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ FirstAngularProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a Test', 'https://image.shutterstock.com/z/stock-photo-healthy-food-clean-eating-selection-fruit-vegetable-seeds-superfood-cereals-leaf-vegetable-on-722718082.jpg')
+  ]
   constructor() { }
 
   ngOnInit() {
